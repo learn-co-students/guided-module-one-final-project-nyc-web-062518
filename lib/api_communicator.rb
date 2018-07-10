0978873c63df05563c6826b2358aa8ddc95b6ab4
jaredianmills <jaredianmills@gmail.com>
2018-07-10 14:46:47 -0400

    created database, added parks table

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,8 +2,5 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-park_data = RestClient.get('https://developer.nps.gov/api/v1/parks?&api_key=mjIjHyHdkl8frtoXIFXG3vmqRXu1C2PqKAkDxjyG')
-park_hash = JSON.parse(park_hash)
-
-binding.pry
-true
+park_data = RestClient.get('https://developer.nps.gov/api/v1/parks?page=2&api_key=mjIjHyHdkl8frtoXIFXG3vmqRXu1C2PqKAkDxjyG&limit=500')
+parks = JSON.parse(park_data)
